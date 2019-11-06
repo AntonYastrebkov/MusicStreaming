7a82ad7fe1af7fba41aecc87225375a2f4d8c1e4
Anton Yastrebkov <anton.yastrebkoff@gmail.com>
2019-11-06 18:33:26 +0300

    Swagger added

--- src/main/java/com/music/streaming/config/WebSecurityConfig.java
+++ src/main/java/com/music/streaming/config/WebSecurityConfig.java
@@ -2,19 +2,19 @@ package com.music.streaming.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/main", "/pain").permitAll()
+                    .antMatchers("/", "/main", "/pain", "/swagger-ui.html").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -23,5 +23,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .logout()
                     .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/swagger-resources/**",
+                "/configuration/**",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 }
--- src/main/java/com/music/streaming/controller/MainController.java
+++ src/main/java/com/music/streaming/controller/MainController.java
@@ -4,10 +4,11 @@ import com.music.streaming.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class MainController {
     private final UserService userService;
 
